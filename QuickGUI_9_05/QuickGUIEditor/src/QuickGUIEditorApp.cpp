edfb3b90691685c162208e0a4ee2f7a6d3bd7c29
barsandcat <barsandcat@localhost>
2009-05-17 18:15:59 +0000

    QuiqGUI ?????????? ??????????, ???????? ????????

--- QuickGUI_9_05/QuickGUIEditor/src/QuickGUIEditorApp.cpp
+++ QuickGUI_9_05/QuickGUIEditor/src/QuickGUIEditorApp.cpp
@@ -4,111 +4,84 @@
 #include "OgreRoot.h"
 
 #include "MainForm.h"
+#include <OgreGLPlugin.h>
 
 namespace QuickGUIEditor
 {
-	QuickGUIEditorApp::QuickGUIEditorApp() :
-		mQuit(false)
-	{
-		mOgreRoot = new Ogre::Root("", "");
+QuickGUIEditorApp::QuickGUIEditorApp() :
+        mQuit(false)
+{
+    mOgreRoot = new Ogre::Root("", "");
 
+    // Gl renedr system
+    mOgreRoot->installPlugin(new Ogre::GLPlugin());
+
+    mRenderSystem = mOgreRoot->getRenderSystemByName("OpenGL Rendering Subsystem");
+    mOgreRoot->setRenderSystem(mRenderSystem);
+
+    // OpenGL Parameters
+
+    //mRenderSystem->setConfigOption( "Colour Depth", "32" );
+    mRenderSystem->setConfigOption("Display Frequency", "N/A");
+    mRenderSystem->setConfigOption("FSAA", "0");
+    mRenderSystem->setConfigOption("Full Screen", "No");
+    mRenderSystem->setConfigOption("RTT Preferred Mode", "FBO");
+    mRenderSystem->setConfigOption("VSync", "No");
+    mRenderSystem->setConfigOption("Video Mode", "1024 x 768");
+
+    mRenderSystem->validateConfigOptions();
+
+    QuickGUI::registerScriptReader();
 #if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-		bool useOpenGL = 0;
-    #if defined _DEBUG
-        mOgreRoot->loadPlugin("RenderSystem_GL_d.dll");
-        mOgreRoot->loadPlugin("RenderSystem_Direct3D9_d.dll");
-    #else
-        mOgreRoot->loadPlugin("RenderSystem_GL.dll");
-        mOgreRoot->loadPlugin("RenderSystem_Direct3D9.dll");
-    #endif
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("resources", "FileSystem");
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("user\\sheets", "FileSystem");
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("user\\skins", "FileSystem");
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("resources\\qgui.textures", "FileSystem");
 #else
-        bool useOpenGL = 1;
-        mOgreRoot->loadPlugin("/usr/local/lib/OGRE/RenderSystem_GL.so");
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../resources", "FileSystem");
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../user/sheets", "FileSystem");
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../user/skins", "FileSystem");
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../resources/qgui.textures", "FileSystem");
 #endif
 
-		if(useOpenGL)
-		{
-			mOgreRoot->setRenderSystem((*(mOgreRoot->getAvailableRenderers()))[0]);
-			mRenderSystem = mOgreRoot->getRenderSystem();
+    Ogre::RenderWindow* w = mOgreRoot->initialise(true);
 
-			// OpenGL Parameters
+    mMainForm = new MainForm(this, mOgreRoot->getAutoCreatedWindow());
 
-			//mRenderSystem->setConfigOption( "Colour Depth", "32" );
-			mRenderSystem->setConfigOption( "Display Frequency", "N/A" );
-			mRenderSystem->setConfigOption( "FSAA", "0" );
-			mRenderSystem->setConfigOption( "Full Screen", "No" );
-			mRenderSystem->setConfigOption( "RTT Preferred Mode", "FBO" );
-			mRenderSystem->setConfigOption( "VSync", "No" );
-			mRenderSystem->setConfigOption( "Video Mode", "1024 x 768" );
-		}
-		else
-		{
-			mOgreRoot->setRenderSystem((*(mOgreRoot->getAvailableRenderers()))[1]);
-			mRenderSystem = mOgreRoot->getRenderSystem();
+    _start();
+}
 
-			// DirectX Parameters
+QuickGUIEditorApp::~QuickGUIEditorApp()
+{
+    delete mMainForm;
 
-			mRenderSystem->setConfigOption( "Allow NVPerfHUD", "No" );
-			mRenderSystem->setConfigOption( "Anti aliasing", "None" );
-			mRenderSystem->setConfigOption( "Floating-point mode", "Fastest" );
-			mRenderSystem->setConfigOption( "Full Screen", "No" );
-			mRenderSystem->setConfigOption( "VSync", "No" );
-			mRenderSystem->setConfigOption( "Video Mode", "1024 x 768 @ 32-bit colour" );
-		}
+    delete mOgreRoot;
+}
 
-		mRenderSystem->validateConfigOptions();
+void QuickGUIEditorApp::_start()
+{
+    Ogre::Timer timer;
 
-		QuickGUI::registerScriptReader();
-#if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("resources", "FileSystem");
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("user\\sheets", "FileSystem");
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("user\\skins", "FileSystem");
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("resources\\qgui.textures.zip", "Zip");
-#else
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../resources", "FileSystem");
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../user/sheets", "FileSystem");
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../user/skins", "FileSystem");
-		Ogre::ResourceGroupManager::getSingleton().addResourceLocation("../resources/qgui.textures.zip", "Zip");
-#endif
+    std::vector<Window*>::iterator it;
 
-		Ogre::RenderWindow* w = mOgreRoot->initialise(true);
+    while (!mQuit)
+    {
+        Ogre::WindowEventUtilities::messagePump();
 
-		mMainForm = new MainForm(this,mOgreRoot->getAutoCreatedWindow());
+        mOgreRoot->renderOneFrame();
 
-		_start();
-	}
+        mMainForm->update(timer.getMicroseconds() / 1000.0);
+        timer.reset();
+    }
+}
 
-	QuickGUIEditorApp::~QuickGUIEditorApp()
-	{
-		delete mMainForm;
+void QuickGUIEditorApp::exit()
+{
+    mQuit = true;
+}
 
-		delete mOgreRoot;
-	}
-
-	void QuickGUIEditorApp::_start()
-	{
-		Ogre::Timer timer;
-
-		std::vector<Window*>::iterator it;
-
-		while(!mQuit)
-		{
-			Ogre::WindowEventUtilities::messagePump();
-
-			mOgreRoot->renderOneFrame();
-
-			mMainForm->update(timer.getMicroseconds() / 1000.0);
-			timer.reset();
-		}
-	}
-
-	void QuickGUIEditorApp::exit()
-	{
-		mQuit = true;
-	}
-
-	bool QuickGUIEditorApp::shuttingDown()
-	{
-		return mQuit;
-	}
+bool QuickGUIEditorApp::shuttingDown()
+{
+    return mQuit;
+}
 }
