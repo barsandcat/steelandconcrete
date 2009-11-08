26da744e8439a4239e89b9b7d70e1b70134050ca
barsandcat <barsandcat@localhost>
2009-11-08 19:54:40 +0000

    ????????? ?????????? ???? ??-?? ??????????? ?????????? ????? ? ????????

--- get_revision.sh
+++ get_revision.sh
@@ -1,8 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 
-echo -n "Finding revision in "
+echo -n "Finding revision "
 pwd
 revnum=`svnversion`
+
+if [ -f src/svn_revision.h ] ; then
+    oldrevnum=`cut -f 2 -d '"' "src/svn_revision.h"`
+    if [ "${revnum}" = "${oldrevnum}" ] ; then
+	    echo "Same revsion as last time: not updating."
+	    exit 0
+    fi
+fi
 
 # Now write the constant declaration to the file:
 echo "#define SVN_REVISION \"$revnum\"" > src/svn_revision.h
