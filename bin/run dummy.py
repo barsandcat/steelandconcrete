e5e162fbf48e5d7fbe5fc94c9e654f81380928ba
barsandcat <barsandcat@localhost>
2010-02-14 13:34:05 +0000

    ??? ??? ??????? ???????

--- bin/run dummy.py
+++ bin/run dummy.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 import os
+import subprocess
+import shutil
+
+shutil.rmtree("log");
+os.mkdir("log");
 
 for i in range(1000):
-	os.spawnlp(os.P_NOWAIT, "./dummy");
+	f = open("log/client" + str(i) + ".log", "w");
+	subprocess.Popen("./dummy", stdout=f, stderr=subprocess.STDOUT, close_fds=True)
+
 
