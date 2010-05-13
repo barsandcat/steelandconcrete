1d5a315acfd5daf794abd493ec42cddbb10bea00
barsandcat <barsandcat@localhost>
2010-05-13 08:47:00 +0300

    ???????? Network (????? ???????? 1000 ???????? ??? ????????). ??????? ????? ????????? ?? asio.

--- src/Network.cpp
+++ src/Network.cpp
@@ -28,23 +28,23 @@ void Network::AllocBuffer(int aSize)
 
 void Network::WriteMessage(const google::protobuf::Message& aMessage)
 {
-    int messageSize = aMessage.ByteSize();
+    size_t messageSize = aMessage.ByteSize();
     AllocBuffer(messageSize);
     aMessage.SerializeToArray(mMessageBuffer, messageSize);
 
     HeaderMsg header;
     header.set_size(messageSize);
-    int headerSize = header.ByteSize();
+    size_t headerSize = header.ByteSize();
     char headerBuffer[HEADER_BUFFER_SIZE];
     header.SerializeToArray(headerBuffer, headerSize);
 
-    if (!mSocket->send(boost::asio::buffer(headerBuffer, headerSize)))
+    if (boost::asio::write(*mSocket, boost::asio::buffer(headerBuffer, headerSize)) != headerSize)
     {
-        throw std::runtime_error("Неудалось записать в сокет загловок!");
+        boost::throw_exception(std::runtime_error("Неудалось записать в сокет загловок!"));
     }
-    if (!mSocket->send(boost::asio::buffer(mMessageBuffer, messageSize)))
+    if (boost::asio::write(*mSocket, boost::asio::buffer(mMessageBuffer, messageSize)) != messageSize)
     {
-        throw std::runtime_error("Неудалось записать в сокет сообщение!");
+        boost::throw_exception(std::runtime_error("Неудалось записать в сокет сообщение!"));
     }
 }
 
@@ -52,22 +52,26 @@ void Network::ReadMessage(google::protobuf::Message& aMessage)
 {
     HeaderMsg header;
     header.set_size(0);
-    int headerSize = header.ByteSize();
+    size_t headerSize = header.ByteSize();
     char headerBuffer[HEADER_BUFFER_SIZE];
-    if (!mSocket->receive(boost::asio::buffer(headerBuffer, headerSize)))
-        throw std::runtime_error("Не удалось прочитать из сокета заголовок!");
+    if (boost::asio::read(*mSocket, boost::asio::buffer(headerBuffer, headerSize)) != headerSize)
+    {
+        boost::throw_exception(std::runtime_error("Не удалось прочитать из сокета заголовок!"));
+    }
     if (!header.ParseFromArray(headerBuffer, headerSize))
-        throw std::runtime_error("Не удалось разобрать заголовок!");
+    {
+        boost::throw_exception(std::runtime_error("Не удалось разобрать заголовок!"));
+    }
 
-    int messageSize = header.size();
+    size_t messageSize = header.size();
     AllocBuffer(messageSize);
 
-    if (!mSocket->receive(boost::asio::buffer(mMessageBuffer, messageSize)))
+    if (boost::asio::read(*mSocket, boost::asio::buffer(mMessageBuffer, messageSize)) != messageSize)
     {
-        throw std::runtime_error("Не удалось прочитать из сокета сообщение!");
+        boost::throw_exception(std::runtime_error("Не удалось прочитать из сокета сообщение!"));
     }
     if (!aMessage.ParseFromArray(mMessageBuffer, messageSize))
     {
-        throw std::runtime_error("Не удалось разобрать сообщение!");
+        boost::throw_exception(std::runtime_error("Не удалось разобрать сообщение!"));
     }
 }
