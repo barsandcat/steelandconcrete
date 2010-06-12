cc2210c618f21b3b882304b77b3030f4cf1355e6
barsandcat <barsandcat@localhost>
2010-06-12 17:42:30 +0300

    ???? ??????, ???????? ????? ????

--- src/UnitTests/UnitListTest.h
+++ src/UnitTests/UnitListTest.h
@@ -11,16 +11,31 @@
 class UnitListTest: public CxxTest::TestSuite
 {
 public:
+
     void TestBase()
     {
+        UnitList::Clear();
         ServerTile tile(Ogre::Vector3::UNIT_X, 0);
         UnitClass unitClass(0, 0 ,0);
+
         ServerUnit& unit = UnitList::NewUnit(tile, unitClass);
         UnitId unitId = unit.GetUnitId();
         ServerUnit* unit2 = UnitList::GetUnit(unitId);
         TS_ASSERT_EQUALS(&unit, unit2);
         UnitList::DeleteUnit(unitId);
         TS_ASSERT(!UnitList::GetUnit(unitId));
+    }
+
+    void TestReuse()
+    {
+        UnitList::Clear();
+        ServerTile tile(Ogre::Vector3::UNIT_X, 0);
+        UnitClass unitClass(0, 0 ,0);
+
+        ServerUnit& unit = UnitList::NewUnit(tile, unitClass);
+        UnitList::DeleteUnit(unit.GetUnitId());
+        UnitList::NewUnit(tile, unitClass);
+        TS_ASSERT_EQUALS(UnitList::GetSize(), 1);
     }
 
 };
