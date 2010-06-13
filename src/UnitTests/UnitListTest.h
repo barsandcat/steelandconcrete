4c437b89a0ded40af116f1076dceea09165d8615
barsandcat <barsandcat@localhost>
2010-06-13 10:03:14 +0300

    ???????? UnitListIterator

--- src/UnitTests/UnitListTest.h
+++ src/UnitTests/UnitListTest.h
@@ -7,6 +7,7 @@
 #include <OgreVector3.h>
 #include <UnitClass.h>
 #include <ServerUnit.h>
+#include <UnitListIterator.h>
 
 class UnitListTest: public CxxTest::TestSuite
 {
@@ -49,6 +50,76 @@ public:
         ServerUnit& unit = UnitList::NewUnit(tile, unitClass);
         TS_ASSERT_DIFFERS(unitId, unit.GetUnitId());
         TS_ASSERT(!UnitList::GetUnit(unitId));
+    }
+
+    void TestIteratorBase()
+    {
+        UnitList::Clear();
+        ServerTile tile(Ogre::Vector3::UNIT_X, 0);
+        UnitClass unitClass(0, 0 ,0);
+
+        UnitList::NewUnit(tile, unitClass);
+        UnitList::NewUnit(tile, unitClass);
+        UnitList::NewUnit(tile, unitClass);
+        int count = 0;
+        for (UnitListIterator i = UnitList::GetIterator(); !i.IsDone(); i.Next())
+        {
+            ++count;
+            TS_ASSERT(i.GetUnit());
+        }
+        TS_ASSERT_EQUALS(count, 3);
+    }
+
+    void TestIteratorSkip()
+    {
+        UnitList::Clear();
+        ServerTile tile(Ogre::Vector3::UNIT_X, 0);
+        UnitClass unitClass(0, 0 ,0);
+
+        UnitList::NewUnit(tile, unitClass);
+        UnitId id = UnitList::NewUnit(tile, unitClass).GetUnitId();
+        UnitList::NewUnit(tile, unitClass);
+        UnitList::DeleteUnit(id);
+        int count = 0;
+        for (UnitListIterator i = UnitList::GetIterator(); !i.IsDone(); i.Next())
+        {
+            ++count;
+            TS_ASSERT(i.GetUnit());
+        }
+        TS_ASSERT_EQUALS(count, 2);
+    }
+
+    void TestIteratorEmpty()
+    {
+        UnitList::Clear();
+
+        int count = 0;
+        for (UnitListIterator i = UnitList::GetIterator(); !i.IsDone(); i.Next())
+        {
+            ++count;
+            TS_ASSERT(i.GetUnit());
+        }
+        TS_ASSERT_EQUALS(count, 0);
+    }
+
+    void TestIteratorEdge()
+    {
+        UnitList::Clear();
+        ServerTile tile(Ogre::Vector3::UNIT_X, 0);
+        UnitClass unitClass(0, 0 ,0);
+
+        UnitId id1 = UnitList::NewUnit(tile, unitClass).GetUnitId();
+        UnitList::NewUnit(tile, unitClass);
+        UnitId id2 = UnitList::NewUnit(tile, unitClass).GetUnitId();
+        UnitList::DeleteUnit(id1);
+        UnitList::DeleteUnit(id2);
+        int count = 0;
+        for (UnitListIterator i = UnitList::GetIterator(); !i.IsDone(); i.Next())
+        {
+            ++count;
+            TS_ASSERT(i.GetUnit());
+        }
+        TS_ASSERT_EQUALS(count, 1);
     }
 
 };
