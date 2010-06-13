4f2f6559690aa0b9777a60cde0f1592800053659
barsandcat <barsandcat@localhost>
2010-06-13 16:59:30 +0300

    ???????? ???? ?? MindList

---
+++ src/UnitTests/MindListTest.h
@@ -0,0 +1,17 @@
+#ifndef MINDLISTTEST_H_INCLUDED
+#define MINDLISTTEST_H_INCLUDED
+
+#include <cxxtest/TestSuite.h>
+
+class MindListTest: public CxxTest::TestSuite
+{
+public:
+    void TestBase()
+    {
+        TS_ASSERT(false);
+    }
+};
+
+
+#endif // MINDLISTTEST_H_INCLUDED
+
