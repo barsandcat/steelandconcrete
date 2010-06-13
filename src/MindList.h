e964316c8d2935bd49e0bc5c41673e4a35e7f069
barsandcat <barsandcat@localhost>
2010-06-13 22:58:27 +0300

    ???? MindList

--- src/MindList.h
+++ src/MindList.h
@@ -1,12 +1,21 @@
 #ifndef MINDLIST_H
 #define MINDLIST_H
 
+#include <boost/ptr_container/ptr_unordered_map.hpp>
+#include <Typedefs.h>
 class Mind;
 
 class MindList
 {
 public:
+    static void NewMind(UnitId aUnitId);
+    static void UpdateMinds(GameTime aPeriod);
+    static void DeleteMind(UnitId aUnitId);
+    static size_t GetSize() {  return mMinds.size(); }
+    static void Clear();
 private:
+    typedef boost::ptr_unordered_map<UnitId, Mind> MindListMap;
+    static MindListMap mMinds;
 };
 
 #endif // MINDLIST_H
