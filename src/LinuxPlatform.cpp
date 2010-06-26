78ab5b968e26d639620a2e5b437f69024f2471d6
barsandcat <barsandcat@localhost>
2010-06-26 21:35:41 +0300

    ?????? ????? ?? ???????

--- src/LinuxPlatform.cpp
+++ src/LinuxPlatform.cpp
@@ -3,11 +3,6 @@
 #include <Platform.h>
 #include <sys/time.h>
 
-extern const char* RU_LOCALE = "ru_RU";
-extern const char* UK_LOCALE = "uk_UA";
-extern const char* EN_LOCALE = "en_US";
-extern const char* JA_LOCALE = "ja_JP";
-
 int64 GetMiliseconds()
 {
     timeval epoch;
--- src/WindowsPlatform.cpp
+++ src/WindowsPlatform.cpp
@@ -2,11 +2,6 @@
 
 #include <Platform.h>
 
-extern const char* RU_LOCALE = "Russian_Russia";
-extern const char* UK_LOCALE = "Ukrainian_Ukraine";
-extern const char* EN_LOCALE = "English_United States";
-extern const char* JA_LOCALE = "Japanese_Japan";
-
 int64 GetMiliseconds()
 {
 	LARGE_INTEGER stop;
