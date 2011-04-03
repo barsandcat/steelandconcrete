97804e1cd677a0c5928ed77c4c919cc8f2fec109
barsandcat <barsandcat@localhost>
2011-04-03 13:58:19 +0300

    New mouse only control

--- ogre1.6.5/Tools/MaterialEditor/MaterialEditor/include/OgreControl.h
+++ ogre1.6.5/Tools/MaterialEditor/MaterialEditor/include/OgreControl.h
@@ -8,13 +8,13 @@ class OgreControl : public wxOgreControl
 {
 public:
     OgreControl(wxWindow* parent, wxWindowID id,
-                const wxPoint& pos, const wxSize& size):
-                wxOgreControl(parent, id, pos, size)
-{
-}
-
-protected:
+                const wxPoint& pos, const wxSize& size);
+    Ogre::SceneManager* CreateSceneManager();
+private:
     virtual void OnMouseMove(wxMouseEvent& event);
+    long m_x;
+    long m_y;
+    Ogre::SceneNode* m_node;
 };
 
 #endif // OGRECONTROL_H
