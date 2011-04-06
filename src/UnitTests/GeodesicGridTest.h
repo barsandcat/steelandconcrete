c43b536a15541f5c608bef20522853fae43d63f7
barsandcat <barsandcat@localhost>
2011-04-06 21:16:11 +0300

    GeodesicGridTest place holder

---
+++ src/UnitTests/GeodesicGridTest.h
@@ -0,0 +1,24 @@
+#ifndef GEODESICGRIDTEST_H_INCLUDED
+#define GEODESICGRIDTEST_H_INCLUDED
+
+#include <cxxtest/TestSuite.h>
+
+class GeodesicGridTest: public CxxTest::TestSuite
+{
+public:
+    void setUp()
+    {
+    }
+
+    void tearDown()
+    {
+    }
+
+    void TestAvatar()
+    {
+        TS_FAIL("!");
+    }
+};
+
+
+#endif // GEODESICGRIDTEST_H_INCLUDED
