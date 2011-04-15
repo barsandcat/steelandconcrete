a0a591b29d8f86dd4e9934a2fed3071430d0ad89
barsandcat <barsandcat@localhost>
2011-04-15 22:39:37 +0300

    Edge now template

--- src/Edge.h
+++ src/Edge.h
@@ -1,19 +1,27 @@
-#ifndef SERVEREDGE_H
-#define SERVEREDGE_H
+#ifndef EDGE_H
+#define EDGE_H
 
-class ServerTile;
-
-class ServerEdge
+template <typename T>
+class Edge: public boost::noncopyable
 {
 public:
-    ServerEdge(ServerTile& aTileA, ServerTile& aTileB);
-    ~ServerEdge();
-    ServerTile& GetTileA() const { return mTileA; }
-    ServerTile& GetTileB() const { return mTileB; }
-protected:
+    Edge(T& aTileA, T& aTileB): mTileA(aTileA), mTileB(aTileB)
+    {
+        mTileA.AddNeighbour(mTileB);
+        mTileB.AddNeighbour(mTileA);
+    }
+
+    ~Edge()
+    {
+        mTileA.RemoveNeighbour(mTileB);
+        mTileB.RemoveNeighbour(mTileA);
+    }
+
+    T& GetTileA() const { return mTileA; }
+    T& GetTileB() const { return mTileB; }
 private:
-    ServerTile& mTileA;
-    ServerTile& mTileB;
+    T& mTileA;
+    T& mTileB;
 };
 
 #endif // SERVEREDGE_H
