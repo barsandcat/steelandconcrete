33d5d281c60d817d4fb438ec294e87c92e76d5b0
barsandcat <barsandcat@localhost>
2011-06-02 21:35:40 +0300

    Async interface

--- src/Network.h
+++ src/Network.h
@@ -3,6 +3,12 @@
 
 #include <INetwork.h>
 #include <google/protobuf/message.h>
+#include <Response.pb.h>
+#include <Request.pb.h>
+#include <boost/function.hpp>
+
+typedef boost::shared_ptr< ResponseMsg > ResponsePtr;
+typedef boost::function< void (ResponsePtr) > ReadCallBack;
 
 class Network: public INetwork
 {
@@ -11,6 +17,7 @@ public:
     ~Network();
     virtual void WriteMessage(const google::protobuf::Message& aMessage);
     virtual void ReadMessage(google::protobuf::Message& aMessage);
+    void AsynReadMessage(ReadCallBack aCallBack) {}
 private:
     void AllocBuffer(int aSize);
     SocketSharedPtr mSocket;
