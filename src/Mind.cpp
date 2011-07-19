bc409840b18a20eb4d1d78173f255281055ffcea
barsandcat <barsandcat@localhost>
2011-07-19 22:07:49 +0300

    Fix avatar unit moving on its own

--- src/Mind.cpp
+++ src/Mind.cpp
@@ -13,19 +13,22 @@ Mind::Mind(UnitId aUnitId):mTarget(NULL), mUnitId(aUnitId), mIsFree(true)
 void Mind::Update(GameTime aPeriod)
 {
     ServerUnit* unit = UnitList::GetUnit(mUnitId);
-    if(mTarget && !UnitList::GetUnit(mTarget->GetUnitId()))
-    {
-        unit->Move(*mTarget);
-        mTarget = NULL;
-        //ChangeList::AddCommandDone(mUnitId);
-    }
-    else
+    if (mIsFree)
     {
         ServerTile& position = unit->GetPosition();
         ServerTile& randomTile = position.GetNeighbour(rand() % position.GetNeighbourCount());
         if(!UnitList::GetUnit(randomTile.GetUnitId()))
         {
             unit->Move(randomTile);
+        }
+    }
+    else
+    {
+        if (mTarget && !UnitList::GetUnit(mTarget->GetUnitId()))
+        {
+            unit->Move(*mTarget);
+            mTarget = NULL;
+            //ChangeList::AddCommandDone(mUnitId);
         }
     }
 }
