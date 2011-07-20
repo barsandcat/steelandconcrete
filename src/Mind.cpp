c471a4da965eace6a05dbd8b42c87e8ea81b4b57
barsandcat <barsandcat@localhost>
2011-07-20 22:32:39 +0300

    Simple target following

--- src/Mind.cpp
+++ src/Mind.cpp
@@ -10,6 +10,20 @@ Mind::Mind(UnitId aUnitId):mTarget(NULL), mUnitId(aUnitId), mIsFree(true)
     //ctor
 }
 
+Ogre::Radian CalcDistance3(const Ogre::Vector3& a, const Ogre::Vector3& b)
+{
+    Ogre::Real lenProduct = a.length() * b.length();
+
+    // Divide by zero check
+    if(lenProduct < 1e-6f)
+        lenProduct = 1e-6f;
+
+    Ogre::Real f = a.dotProduct(b) / lenProduct;
+
+    f = Ogre::Math::Clamp(f, (Ogre::Real)-1.0, (Ogre::Real)1.0);
+    return Ogre::Math::ACos(f);
+}
+
 void Mind::Update(GameTime aPeriod)
 {
     ServerUnit* unit = UnitList::GetUnit(mUnitId);
@@ -24,11 +38,30 @@ void Mind::Update(GameTime aPeriod)
     }
     else
     {
-        if (mTarget && !UnitList::GetUnit(mTarget->GetUnitId()))
+        if (mTarget)
         {
-            unit->Move(*mTarget);
-            mTarget = NULL;
-            //ChangeList::AddCommandDone(mUnitId);
+            ServerTile& currentTile = unit->GetPosition();
+            ServerTile* currentTarget = &currentTile.GetNeighbour(0);
+            Ogre::Radian currentDist = CalcDistance3(currentTarget->GetPosition(), mTarget->GetPosition());
+            for (size_t i = 1; i < currentTile.GetNeighbourCount(); ++i)
+            {
+                Ogre::Radian dist = CalcDistance3(currentTile.GetNeighbour(i).GetPosition(), mTarget->GetPosition());
+                if (dist < currentDist)
+                {
+                    currentTarget = &currentTile.GetNeighbour(i);
+                    currentDist = dist;
+                }
+            }
+
+            if (!UnitList::GetUnit(currentTarget->GetUnitId()))
+            {
+                unit->Move(*currentTarget);
+                if (mTarget == currentTarget)
+                {
+                    mTarget = NULL;
+                    //ChangeList::AddCommandDone(mUnitId);
+                }
+            }
         }
     }
 }
