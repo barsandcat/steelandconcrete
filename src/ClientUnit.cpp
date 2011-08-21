816a3b7f76ef7b21212568e66b8db9858a2dfc9a
barsandcat <barsandcat@localhost>
2011-08-21 14:41:41 +0300

    Client unit can not have null mTile

--- src/ClientUnit.cpp
+++ src/ClientUnit.cpp
@@ -6,12 +6,13 @@
 #include <VisualCodes.h>
 #include <ClientTile.h>
 
-ClientUnit::ClientUnit(UnitId aUnitId, uint32 aVisual):
-    mTile(NULL),
+ClientUnit::ClientUnit(UnitId aUnitId, uint32 aVisual, ClientTile* aTile):
+    mTile(aTile),
     mEntity(NULL),
     mUnitId(aUnitId),
     mVisualCode(aVisual)
 {
+    assert(mTile);
     mNode = ClientApp::GetSceneMgr().getRootSceneNode()->createChildSceneNode();
     Ogre::String indexName = Ogre::StringConverter::toString(mUnitId);
     mEntity = ClientApp::GetSceneMgr().createEntity(indexName + "Unit.entity", GetMesh(mVisualCode));
@@ -24,29 +25,18 @@ ClientUnit::~ClientUnit()
     ClientGame::EraseUnitId(mUnitId);
     ClientApp::GetSceneMgr().destroyEntity(mEntity);
     ClientApp::GetSceneMgr().destroySceneNode(mNode);
-    if (mTile)
-    {
-        mTile->RemoveUnit();
-    }
+    mTile->RemoveUnit();
     mEntity = NULL;
     mNode = NULL;
 }
 
 void ClientUnit::SetTile(ClientTile* aTile)
 {
-    if (mTile)
-    {
-        mTile->RemoveUnit();
-        mTile = NULL;
-    }
-
-    if (aTile)
-    {
-        aTile->SetUnit(this);
-        mTile = aTile;
-        const Ogre::Vector3 pos = aTile->GetGridNode().GetPosition();
-        mNode->setDirection(pos.normalisedCopy(), Ogre::Node::TS_WORLD, Ogre::Vector3::UNIT_Z);
-        mNode->setPosition(pos);
-    }
-
+    assert(aTile);
+    mTile->RemoveUnit();
+    aTile->SetUnit(this);
+    mTile = aTile;
+    const Ogre::Vector3 pos = aTile->GetGridNode().GetPosition();
+    mNode->setDirection(pos.normalisedCopy(), Ogre::Node::TS_WORLD, Ogre::Vector3::UNIT_Z);
+    mNode->setPosition(pos);
 }
