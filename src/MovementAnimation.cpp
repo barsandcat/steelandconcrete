96b90cd404b848262fcebaf0e239038aa5bf028d
barsandcat <barsandcat@localhost>
2011-08-21 21:09:59 +0300

    MovementAnimation plugged in

--- src/MovementAnimation.cpp
+++ src/MovementAnimation.cpp
@@ -1,16 +1,18 @@
 #include <pch.h>
+
 #include <MovementAnimation.h>
+#include <ClientLog.h>
 
 MovementAnimation::MovementAnimation(const Ogre::Vector3& aOrigin, const Ogre::Vector3& aDestination):
-    mOrigin(aOrigin.getRotationTo(Ogre::Vector3::UNIT_Z)),
-    mDestination(aDestination.getRotationTo(Ogre::Vector3::UNIT_Z)),
+    mOrigin(Ogre::Vector3::UNIT_Z.getRotationTo(aOrigin)),
+    mDestination(Ogre::Vector3::UNIT_Z.getRotationTo(aDestination)),
     mProgress(0)
 {
 }
 
 void MovementAnimation::Update(FrameTime aFrameTime)
 {
-    const Ogre::Real speed = 1 / 500000;
+    const Ogre::Real speed = 1.0f / 50000.0f;
     mProgress += aFrameTime * speed;
 }
 
