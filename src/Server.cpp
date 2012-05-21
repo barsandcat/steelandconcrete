2b8999285cd2709aad4ff437c49b37d6f8c0912c
barsandcat <barsandcat@localhost>
2012-05-21 12:11:36 +0300

    Remove tclap

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,13 +1,16 @@
 #include <pch.h>
 
 #include <ServerGame.h>
-#include <tclap/CmdLine.h>
 #include <gflags/gflags.h>
 
 #include <ReleaseVersion.h>
 #include <ProtocolVersion.h>
 
 DEFINE_bool(short_version, false, "Short version to use in build scripts");
+DEFINE_string(address, "localhost", "Server address");
+DEFINE_int32(port, 4512, "Port");
+DEFINE_int32(size, 4, "Map size: 1 - 162, 2 - 642, 3 - 2562, 4 - 10242, 5 - 40962, 6 - 163842, 7 - 655362 tiles");
+
 
 void RunServer(int argc, char **argv)
 {
@@ -16,28 +19,7 @@ void RunServer(int argc, char **argv)
 	// Define the command line object.
 	Ogre::String version = Ogre::StringConverter::toString(PROTOCOL_VERSION) +
 		"." + Ogre::StringConverter::toString(RELEASE_VERSION);
-	TCLAP::CmdLine cmd("Steelandconcrete server", ' ', version.c_str());
 
-	TCLAP::ValueArg<Ogre::String> address("a", "address", "Server address", false, "localhost", "string");
-	cmd.add(address);
-
-	TCLAP::ValueArg<int32> port("p", "port", "Port", false, 4512, "int");
-	cmd.add(port);
-
-	TCLAP::ValueArg<int32> size("s", "size",
-		"Map size: 1 - 162, 2 - 642, 3 - 2562, 4 - 10242, 5 - 40962, 6 - 163842, 7 - 655362 tiles",
-		false, 4, "int");
-	cmd.add(size);
-
-	TCLAP::ValueArg<int32> seaLevel("l", "sealevel",
-		"Sea level: 0-10000",
-		false, 5000, "int");
-	cmd.add(seaLevel);
-
-	//TCLAP::SwitchArg shortVersion("v","short_version","Short version to use in build scripts", cmd, false);
-
-	// Parse the args.
-	cmd.parse( argc, argv );
 
 	if (FLAGS_short_version)
     {
@@ -45,6 +27,6 @@ void RunServer(int argc, char **argv)
         return;
     }
 
-	ServerGame app(size.getValue());
-	app.MainLoop(address.getValue(), port.getValue());
+	ServerGame app(FLAGS_size);
+	app.MainLoop(FLAGS_address, FLAGS_port);
 }
