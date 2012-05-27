35e15e049de09eefbf25b272641ee17203278513
barsandcat <barsandcat@localhost>
2012-05-27 22:31:11 +0300

    Load flags from steelandconcrete.flags in current directory (if absent from home/.steelandconcrete directory)

--- src/Linux.cpp
+++ src/Linux.cpp
@@ -2,19 +2,36 @@
 #include <ClientApp.h>
 
 #include <Platform.h>
+#include <boost/filesystem/operations.hpp>
 
 int main(int argc, char **argv)
 {
     google::InstallFailureSignalHandler();
-    google::InitGoogleLogging(argv[0]);
-    FLAGS_alsologtostderr = true;
-    google::ParseCommandLineFlags(&argc, &argv, true);
 
     // Create application object
     try
     {
         Ogre::String home = getenv("HOME");
         CheckConfigFile(home);
+
+        Ogre::String config = "steelandconcrete.flags";
+        if (boost::filesystem::exists(config))
+        {
+            FLAGS_flagfile = config;
+        }
+        else
+        {
+            config = home + "/.steelandconcrete/steelandconcrete.flags";
+            if (boost::filesystem::exists(config))
+            {
+                FLAGS_flagfile = config;
+            }
+        }
+
+
+        google::ParseCommandLineFlags(&argc, &argv, true);
+        google::InitGoogleLogging(argv[0]);
+
         ClientApp app(home + "/.steelandconcrete/steelandconcrete1.cfg");
         LOG(INFO) << "ClientApp ready";
         app.MainLoop();
