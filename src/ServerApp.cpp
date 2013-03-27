57037501fa3f783b97b14c0367c596e2add90451
barsandcat <barsandcat@localhost>
2013-03-27 21:39:55 +0200

    Merge in curseslab: add pdcurs34 lib, add TUI to server

--- src/ServerApp.cpp
+++ src/ServerApp.cpp
@@ -7,11 +7,208 @@
 #include <ProtocolVersion.h>
 #include <ConnectionManager.h>
 
+#ifndef _XOPEN_SOURCE_EXTENDED
+# define _XOPEN_SOURCE_EXTENDED 1
+#endif
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <curses.h>
+#include <locale.h>
+#include <sstream>
+#include <vector>
+
 DEFINE_bool(short_version, false, "Short version to use in build scripts");
 DEFINE_string(address, "localhost", "Server address");
 DEFINE_int32(port, 4512, "Port");
 DEFINE_int32(size, 4, "Map size: 1 - 162, 2 - 642, 3 - 2562, 4 - 10242, 5 - 40962, 6 - 163842, 7 - 655362 tiles");
 
+
+
+void RunCloseMenu() {}
+void RunAddUser() {}
+void RunKillServer()
+{
+    throw 1;
+}
+
+class StatusWindow
+{
+public:
+    StatusWindow()
+    {
+        mWin = newwin(1, COLS, LINES - 1, 0);
+        wbkgd(mWin, A_REVERSE);
+    }
+    ~StatusWindow()
+    {
+        delwin(mWin);
+    }
+    void Update()
+    {
+        wclear(mWin);
+        mvwaddstr(mWin, 0, 0, "Local server");
+        wrefresh(mWin);
+    }
+    void Redraw()
+    {
+        touchwin(mWin);
+    }
+private:
+    WINDOW* mWin;
+};
+
+class LogWindow
+{
+public:
+    LogWindow()
+    {
+        mWin = newwin(LINES - 1, COLS, 0, 0);
+        scrollok(mWin, TRUE);
+        counter = 0;
+    }
+
+    ~LogWindow()
+    {
+        delwin(mWin);
+    }
+
+    void Update()
+    {
+        counter++;
+        std::stringstream ss;
+        ss << '\n' << "Just some log line " << counter;
+        waddstr(mWin, ss.str().c_str());
+        wrefresh(mWin);
+    }
+
+    void Redraw()
+    {
+        touchwin(mWin);
+    }
+private:
+    WINDOW* mWin;
+    int counter;
+};
+
+class MenuWindow
+{
+private:
+    typedef std::pair<std::string, void (*)()> Command;
+    typedef std::vector<Command> CommandVector;
+public:
+    MenuWindow()
+    {
+        mWin = newwin(LINES, COLS, 0, 0);
+        wborder(mWin, 0, 0, 0, 0, 0, 0, 0, 0);
+        mOption = 0;
+        mCommands.push_back(Command("Close menu", RunCloseMenu));
+        mCommands.push_back(Command("Add user", RunAddUser));
+        mCommands.push_back(Command("Kill server", RunKillServer));
+    }
+
+    ~MenuWindow()
+    {
+        delwin(mWin);
+    }
+
+    void Run()
+    {
+        touchwin(mWin);
+
+        while (true)
+        {
+            nodelay(mWin, false);
+            noecho();
+            keypad(stdscr, true);
+            raw();
+
+            Update();
+
+            switch(getch())
+            {
+            case 10:
+            case 13:
+            case KEY_ENTER:
+                (*mCommands[mOption].second)();
+                touchwin(mWin);
+                break;
+
+            case KEY_PPAGE:
+            case KEY_HOME:
+                mOption = 0;
+                break;
+
+            case KEY_NPAGE:
+            case KEY_END:
+                mOption = mCommands.size() - 1;
+                break;
+
+            case KEY_UP:
+                mOption = (mOption == 0) ? mOption : mOption - 1;
+                break;
+
+            case KEY_DOWN:
+                mOption = (mOption == mCommands.size() - 1) ? mOption : mOption + 1;
+                break;
+            case 'Q':
+            case 'q':
+                return;
+            }
+        }
+    }
+
+private:
+    void Update()
+    {
+        for (int i = 0; i < mCommands.size(); ++i)
+        {
+            wattrset(mWin, i == mOption ? A_REVERSE : A_NORMAL);
+            mvwaddstr(mWin, 1 + i, 1, mCommands[i].first.c_str());
+        }
+        wrefresh(mWin);
+    }
+    int mOption;
+    WINDOW* mWin;
+    CommandVector mCommands;
+};
+
+void RunTUI()
+{
+    StatusWindow statusWindow;
+    LogWindow logWindow;
+    MenuWindow menuWindow;
+
+    while (true)
+    {
+        noecho();
+        curs_set(0);
+        timeout(200);
+        keypad(stdscr, TRUE);
+        raw();
+
+        logWindow.Update();
+        statusWindow.Update();
+
+        switch(getch())
+        {
+        case 10:
+        case 13:
+        case KEY_ENTER:
+        case KEY_PPAGE:
+        case KEY_HOME:
+        case KEY_NPAGE:
+        case KEY_END:
+        case KEY_UP:
+        case KEY_DOWN:
+            menuWindow.Run();
+            logWindow.Redraw();
+            statusWindow.Redraw();
+            break;
+        }
+    }
+}
 
 void GameLoop(ServerGame& aGame)
 {
@@ -38,17 +235,31 @@ void Run(int argc, char **argv, const bool& aContinue)
     }
     else
     {
+        setlocale(LC_ALL, "");
+
+#ifdef XCURSES
+        Xinitscr(argc, argv);
+#else
+        initscr();
+#endif
+
         ServerGame game(FLAGS_size);
         boost::thread cm(ConnectionManager, boost::ref(game), FLAGS_address, FLAGS_port);
         boost::thread ml(GameLoop, boost::ref(game));
-        while (aContinue)
+        try
         {
-            boost::this_thread::sleep(boost::posix_time::milliseconds(200));
+            RunTUI();
+        }
+        catch(...)
+        {
         }
         cm.interrupt();
         ml.interrupt();
         ml.join();
+
+        endwin();
     }
 
-	google::ShutdownGoogleLogging();
+    google::ShutdownGoogleLogging();
 }
+
