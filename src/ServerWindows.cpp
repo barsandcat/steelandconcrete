a5c9adb3bb7a44944e01cffa747cd1ba3366a2ed
barsandcat <barsandcat@localhost>
2013-04-08 13:52:04 +0300

    Windows build fixes

--- src/ServerWindows.cpp
+++ src/ServerWindows.cpp
@@ -7,8 +7,7 @@ int main(int argc, char **argv)
 {
     try
     {
-        bool continueRun = true;
-        Run(argc, argv, continueRun);
+        Run(argc, argv);
     }
     catch (std::exception& e)
     {
